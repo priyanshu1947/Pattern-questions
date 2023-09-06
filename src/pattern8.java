0bf8a1e3d7c3bcea2c62af59cf5ad536ea2df924
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-06 10:44:27 +0530

    pattern 8 gfg

---
+++ src/pattern8.java
@@ -0,0 +1,22 @@
+public class pattern8 {
+    public static void main(String[] args) {
+
+
+
+    }
+    void printTriangle(int n)
+
+    {
+        for (int i =1; i<=n;i++)
+        {
+            for(int j =1;j<n-i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // code here
+    }
+}
+
