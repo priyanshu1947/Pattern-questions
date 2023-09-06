b5d1caf64f7a7f2e178721f0550a1e0f9dd5794b
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-06 10:29:55 +0530

    pattern 1

---
+++ src/pattern1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void print1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        print1(n);
+    }
+}
