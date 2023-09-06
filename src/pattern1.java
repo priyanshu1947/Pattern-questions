40852f89b1e36753522b97f3e0411f1e1d7ea8c0
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-06 10:36:08 +0530

    pattern 1 gfg

--- src/pattern1.java
+++ src/pattern1.java
@@ -1,17 +1,8 @@
+class Solution {
 
-
-class pattern1 {
-
-    void printTriangle(int n) {
-
-        for(int i=1;i<=n;++i){
-            for(int j=1;j<=i;++j){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        for(int i=n-1;i>=1;--i){
-            for(int j=1;j<=i;++j){
+    void printSquare(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<n-i+1; j++){
                 System.out.print("* ");
             }
             System.out.println();
