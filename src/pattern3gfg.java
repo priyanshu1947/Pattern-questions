d6c0b52ce525abdbdbdded4003f6af44417ce3ec
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-06 10:39:18 +0530

    pattern 3 gfg

---
+++ src/pattern3gfg.java
@@ -0,0 +1,15 @@
+public class pattern3gfg
+{
+    public static void main(String[] args) {
+
+    }
+    void printSquare(int n) {
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<n-i+1; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
