686aa1d76392197168b48737f9899cdbd52cb58f
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-06 10:45:17 +0530

    pattern 9 gfg

---
+++ src/pattern9.java
@@ -0,0 +1,20 @@
+public class pattern9 {
+    public static void main(String[] args) {
+
+    }
+    void printTriangle(int n)
+
+    {
+        for (int i =1; i<=n;i++)
+        {
+            for(int j =1;j<n-i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // code here
+    }
+}
+
